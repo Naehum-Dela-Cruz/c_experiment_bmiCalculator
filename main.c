9c627b91c0ecb2afdcc7ae1d2684bdbf42e800e2
Naehum-Dela-Cruz <153443675+Naehum-Dela-Cruz@users.noreply.github.com>
2024-02-05 08:01:18 +0000

    finish working code

--- main.c
+++ main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
 
+  char uName[25];
+  int age, height, weight;
+  double bmi;
+  
+  printf("Enter Name: ");
+  fgets(uName, sizeof(uName), stdin);
 
+  printf("Input age: ");
+  scanf("%d", &age);
 
+  printf("Input height (inches): ");
+  scanf("%d", &height);
+  
+  printf("Input weight (lbs): ");
+  scanf("%d", &weight);
+
+  bmi = (703 * weight) / (height * height);
+
+  printf("\nName: %s", uName);
+
+  printf("Age: %d\n", age);
+
+  printf("Height: %d\n", height);
+
+  printf("Weight: %d\n", weight);
+
+  printf("BMI: %lf\n", bmi);
+
+  if(bmi < 18.5){
+    printf("Underweight");
+  }
+  else if(bmi >= 18.5 && bmi < 25){
+    printf("Normal");
+  }
+  else if (bmi >= 25 && bmi < 30){
+    printf("Overweight");
+  }
+  else{
+    printf("Obese");
+  }
   
   return 0;
 }
